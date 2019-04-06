0a8c07fb44d8d64c2f199c39fe1db6118b747645
milad <milad.khaari@gmail.com>
2019-04-06 19:13:01 +0430

    added radio buttons

--- user.js
+++ user.js
@@ -31,10 +31,22 @@ $(document).ready(function() {
       for (user of users) {
         console.log(user.id);
         let str = "<tr>";
+
+        str += "<td>" + "<input class='radioButtons' type='radio' name='choose' value='" + user.id + "'>" + "</td>";
+
+
         str += "<td>" + user.id + "</td>";
-        str += "<td>" + user.first_name + "</td>";
-        str += "<td>" + user.last_name + "</td>";
-        str += "<td> <img src='" + user.avatar + "' height='42' width='42'/></td>";
+        str += "<td>" + user.first_name + " " + user.last_name + "</td>";
+        str += "<td> <img src='" + user.avatar + "'/></td>";
+/*
+        //not sure if it's right(good) to do this! for sepration of concearns
+        str += "<p>"ok
+        str += "<input type='radio' name='choose'>";
+        str += "</p>"
+*/
+        // str += "<td>" + "mark " + user.last_name + "?" + "<input type='radio' name='choose'>" + "</td>";
+
+
         str += "</tr>";
         $("#user-list > tbody:last-child").append(str);
       }
@@ -59,12 +71,74 @@ $(document).ready(function() {
             $('.content').html('<div class="error">Error fetching data</div>');
             console.log('Error: ' + response.statusText);
           });
-    }    
+    }
 });
 
+
+
   function selectBtn(task){
-    $('.hidden').hide();
+    dealWithRadioButtons(task);
+    dealWithHints();
+    showTheRightForm(task);
+
+    function dealWithRadioButtons(task){
+      //uncheck all:
+      $(".radioButtons").prop("checked", false);
+
+      if(task == 'add'){
+        hideRadioButton();
+      }
+      else if(task == 'edit') {
+        showRadioButton();
+      }
+      else if(task == 'remove') {
+        showRadioButton();
+      }
+    }
+
+    function showRadioButton(){
+      $('.radioButtons').css('visibility','visible');
+    }
     
-    task = '#'+task;
-    $(task).show();
+    function hideRadioButton(){
+      // $('#user-list tbody tr td:first-child').css('visibility','hidden');
+      $('.radioButtons').css('visibility','hidden');
+    }
+
+    function dealWithHints(){
+      $('.beforeHint').show();
+      $('.afterHint').hide();
+
+      // const radioButton = $('#user-list tbody tr td:first-child');
+      $('.radioButtons').click(function() {
+        $('.beforeHint').hide();
+        $('.afterHint').show();
+      }); 
+    } 
+
+    function showTheRightForm(task){
+      $('.hiddenForms').hide();
+      $('#'+task).show();
+    }
+
   }
+
+
+/*  function edit(task){
+    showRadioButton();
+  }
+
+  function remove(task){
+    showRadioButton();
+  }
+
+  function suckIt() {
+    console.log('Error: ' + response.statusText);
+
+    var children = tableFields.children;
+    for (var i = 0; i < children.length; i++) {
+      var tableChild = children[i];
+      // Do stuff
+    }
+  }
+*/
