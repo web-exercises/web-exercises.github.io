c0f22b9e507f81058591d9fdb44b6e33ab0f5137
milad <milad.khaari@gmail.com>
2019-04-06 20:16:41 +0430

    small changes in appearance.

--- user.js
+++ user.js
@@ -31,29 +31,15 @@ $(document).ready(function() {
       for (user of users) {
         console.log(user.id);
         let str = "<tr>";
-
-        str += "<td>" + "<input class='radioButtons' type='radio' name='choose' value='" + user.id + "'>" + "</td>";
-
-
+        str += "<td>" + "<input class='radioButtons' type='radio' name='choose' value='"
+         + user.id + "'>" + "</td>";
         str += "<td>" + user.id + "</td>";
         str += "<td>" + user.first_name + " " + user.last_name + "</td>";
         str += "<td> <img src='" + user.avatar + "'/></td>";
-/*
-        //not sure if it's right(good) to do this! for sepration of concearns
-        str += "<p>"ok
-        str += "<input type='radio' name='choose'>";
-        str += "</p>"
-*/
-        // str += "<td>" + "mark " + user.last_name + "?" + "<input type='radio' name='choose'>" + "</td>";
-
-
         str += "</tr>";
         $("#user-list > tbody:last-child").append(str);
       }
     }
-
-
-
 
 
     function submit(){
@@ -101,15 +87,14 @@ $(document).ready(function() {
     }
     
     function hideRadioButton(){
-      // $('#user-list tbody tr td:first-child').css('visibility','hidden');
       $('.radioButtons').css('visibility','hidden');
+      // or: $('#user-list tbody tr td:first-child').css('visibility','hidden');
     }
 
     function dealWithHints(){
       $('.beforeHint').show();
       $('.afterHint').hide();
 
-      // const radioButton = $('#user-list tbody tr td:first-child');
       $('.radioButtons').click(function() {
         $('.beforeHint').hide();
         $('.afterHint').show();
@@ -123,22 +108,3 @@ $(document).ready(function() {
 
   }
 
-
-/*  function edit(task){
-    showRadioButton();
-  }
-
-  function remove(task){
-    showRadioButton();
-  }
-
-  function suckIt() {
-    console.log('Error: ' + response.statusText);
-
-    var children = tableFields.children;
-    for (var i = 0; i < children.length; i++) {
-      var tableChild = children[i];
-      // Do stuff
-    }
-  }
-*/
