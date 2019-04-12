dfa0813ab9ea84670be36399898f982dbad76ecd
milad <milad.khaari@gmail.com>
2019-04-12 15:12:16 +0430

    added submitMember

--- user.js
+++ user.js
@@ -1,6 +1,6 @@
 // 1. input form
 // 2. patch post
-// 3. on seccess(2nd .then):  update local storage
+// 3. on success(on second ".then"):  update local storage
 // 4. call fillUsers and avoid redundancy
 
 $(document).ready(function() {
@@ -26,39 +26,118 @@ $(document).ready(function() {
         fillUsers(JSON.parse(users));
     }
 
+});
+
+/////////////////  end of  $(document).ready()  //////////////////
+
+
+
+
+
     function fillUsers(users){
       $('#loading').remove();
       for (user of users) {
-        console.log(user.id);
-        let str = "<tr>";
-        str += "<td>" + "<input class='radioButtons' type='radio' name='choose' value='"
-         + user.id + "'>" + "</td>";
-        str += "<td>" + user.id + "</td>";
-        str += "<td>" + user.first_name + " " + user.last_name + "</td>";
-        str += "<td> <img src='" + user.avatar + "'/></td>";
-        str += "</tr>";
-        $("#user-list > tbody:last-child").append(str);
+        // console.log(user.id);
+        attachUser(user);
       }
     }
 
+    function attachUser(user){
+      let str = "<tr>";
+      str += "<td> <input class='radioButtons' type='radio' name='choose' value='" + user.id + "'></td>";
+      str += "<td>" + user.id + "</td>";
+      str += "<td>" + user.first_name + " " + user.last_name + "</td>";
+      str += "<td> <img src='" + user.avatar + "'/></td>";
+      str += "</tr>";
+      $("#user-list > tbody:last-child").append(str);
+}
 
-    function submit(){
 
-      fetch('https://www.reqres.in/api/users')
-        .then(function(response) {
-            return response.json();
-          })
-        .then(function(response) {
-            users = response.data;
-            localStorage.setItem('user-list', JSON.stringify(users));
-            fillUsers(users);
-          })
-        .catch(function(response) {
-            $('.content').html('<div class="error">Error fetching data</div>');
-            console.log('Error: ' + response.statusText);
-          });
+
+// under construction: 
+    function submitMember(){
+
+      url = 'https://www.reqres.in/api/users';
+
+      const name = $("#memberName").val();
+      const job = $("#memberJob").val();
+        console.log(job);
+        console.log(name);
+
+      newMember = {
+        "name": name,
+        "job": job
+      }
+
+      options = {
+        method: 'POST',  
+        headers: {},  
+        body: JSON.stringify(newMember)
+      }
+
+/*
+      fetch(url, options)
+      .then(function (data) {  
+        console.log('Request success: ', data);
+      })
+      .then(function (data) {  
+        console.log('Great!');
+      }) 
+      .catch(function (error) {  
+        console.log('Request failure: ', error);  
+      });
+*/
+
+      //on success:
+      addNewMember();
+
+
+      function addNewMember(){
+
+            if (localStorage.membercount) {
+              localStorage.membercount = Number(localStorage.membercount)+1;
+            } else {
+              localStorage.membercount = 100;
+            }
+
+          newMember.id = Number(localStorage.membercount);
+          newMember.first_name = newMember.name;
+          newMember.last_name = "";
+          let randomNumber = Math.floor(Math.random() * 10) + 1;
+          newMember.avatar = "avatars/" + randomNumber + ".png";
+
+
+
+          let oldItems = JSON.parse(localStorage.getItem('user-list')) || [];
+          oldItems.push(newMember);
+
+
+          localStorage.setItem('user-list', JSON.stringify(oldItems));
+
+    /*
+          $("#user-list > tbody").html("");
+          fillUsers(oldItems);
+    */
+
+
+
+
+
+          attachUser(newMember)
+       }
+
+
     }
-});
+// End of under construction section.
+
+
+
+
+
+
+
+
+
 
 
 
